df1ca4e1332957526b10c6b8db2390f9f2a0a1ab
iahmedfarag <ahmedfarag1k@gmail.com>
2023-04-27 18:21:39 +0200

    netlify payment issue

--- functions/create-payment-intent.js
+++ functions/create-payment-intent.js
@@ -4,33 +4,26 @@ require("dotenv").config();
 const stripe = require("stripe")(process.env.VITE_REACT_APP_STRIPE_SECRET_KEY);
 
 exports.handler = async function (event, context) {
-  if (event.body) {
-    const { cart, shippingFee, totalAmount } = JSON.parse(event.body);
-    const calculateOrderAmount = () => {
-      return shippingFee + totalAmount;
-    };
+  const { cart, shippingFee, totalAmount } = JSON.parse(event.body);
+  const calculateOrderAmount = () => {
+    return shippingFee + totalAmount;
+  };
 
-    try {
-      const paymentIntent = await stripe.paymentIntents.create({
-        amount: calculateOrderAmount(),
-        currency: "usd",
-      });
-      return {
-        statusCode: 200,
-        body: JSON.stringify({
-          clientSecret: paymentIntent.client_secret,
-        }),
-      };
-    } catch (error) {
-      return {
-        statusCode: 500,
-        body: JSON.stringify({ msg: error.message }),
-      };
-    }
-  } else {
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: calculateOrderAmount(),
+      currency: "usd",
+    });
     return {
       statusCode: 200,
-      body: "Create payment intent",
+      body: JSON.stringify({
+        clientSecret: paymentIntent.client_secret,
+      }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 501,
+      body: JSON.stringify({ msg: error.message }),
     };
   }
 };
