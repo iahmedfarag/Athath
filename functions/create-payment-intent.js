fe262deb21317ad9c2a109f2b6d867696d6e7d3f
iahmedfarag <ahmedfarag1k@gmail.com>
2023-04-27 19:51:41 +0200

    payment

--- functions/create-payment-intent.js
+++ functions/create-payment-intent.js
@@ -19,6 +19,7 @@ exports.handler = async function (event, context) {
     });
     return {
       statusCode: 200,
+      apiKey: process.env.VITE_REACT_APP_STRIPE_SECRET_KEY,
       body: JSON.stringify({
         clientSecret: paymentIntent.client_secret,
       }),
@@ -26,9 +27,7 @@ exports.handler = async function (event, context) {
   } catch (error) {
     return {
       statusCode: 501,
-      body: JSON.stringify({
-        clientSecret: paymentIntent.client_secret,
-      }),
+      body: JSON.stringify({ msg: error.message }),
     };
   }
 };
